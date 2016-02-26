6a2d8501db18d367722b6b24067bddc1032a1d8b
washigeri <malek.elouerghi@gmail.com>
2016-02-26 14:49:54 +0100

    tests

--- src/main.c
+++ src/main.c
@@ -3,9 +3,11 @@
 #include "snake.h"
 
 int main(){
-    snake s=init_snake(2);
+    snake s=init_snake(4);
     s.pos[0].x=10;s.pos[0].y=10;
     s.pos[1].x=9;s.pos[1].y=10;
+    s.pos[2].x=8; s.pos[2].y=10;
+    s.pos[3].x=7; s.pos[3].y=10;
     plateau p=init_plateau(20);
     affiche(p,s);
     while(!jouer(s,p)){
