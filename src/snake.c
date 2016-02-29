2a32f4341cc1e04ff497428c1e3b8f7555c78dce
washigeri <malek.elouerghi@gmail.com>
2016-02-29 14:04:02 +0100

    fix

---
+++ src/snake.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+
+snake init_snake(int len){
+    snake res;
+    res.pos=(coord*)malloc(len*sizeof(coord));
+    int i;
+    for(i=0;i<len;i++){
+            res.pos[i].x=0;
+            res.pos[i].y=0;
+    }
+    res.taille=len;
+    res.dir=(direction *) malloc (sizeof(direction));
+    res.dir[0]=right;
+    return res;
+}
+
