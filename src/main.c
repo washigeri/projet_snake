74c4073904117855b09b815fb537e38afdc55adb
marcolinobe <bergdollmarc04@gmail.com>
2016-03-07 15:53:10 +0100

    ajout interface

--- src/main.c
+++ src/main.c
@@ -8,12 +8,14 @@
 
 int main()
 {
+/*LE SNAKE*/
     snake snak=init_snake(4);
     snak.pos[0].x=10;snak.pos[0].y=10;
     snak.pos[1].x=9;snak.pos[1].y=10;
     snak.pos[2].x=8;snak.pos[2].y=10;
     snak.pos[3].x=7;snak.pos[3].y=10;
-    snake schlanga=init_snake(4);
+/*LE SCHLANGA*/ 
+   snake schlanga=init_snake(4);
     schlanga.pos[0].x=15;schlanga.pos[0].y=10;
     schlanga.pos[1].x=16;schlanga.pos[1].y=10;
     schlanga.pos[2].x=17;schlanga.pos[2].y=10;
@@ -22,8 +24,26 @@ int main()
     snake* s=(snake*) malloc (2*sizeof(snake));
     s[0]=snak;
     s[1]=schlanga;
-    plateau p=init_plateau(30);
+
+/*INITILAISATION*/
+	/*DEBUT DU JEU*/
+
+	system("clear");
+	int taille_plateau=0;
+	printf("\n****LE JEU VA COMMENCER****\n\n");
+	printf("\nSaisissez la taille_plateau du tableau (minimum 20x20) puis ENTER\n");
+
+	scanf("%d",&taille_plateau);
+	while( taille_plateau<=19 ){printf("\nResaisissez la taille_plateau:");scanf("%d",&taille_plateau);}
+
+	printf("\nReady!\n");
+	printf("Pour se deplacer: appuyer sur z,q,s et d\n");
+	system("sleep 5;clear");
+
+    plateau p=init_plateau(taille_plateau);
     affiche(p,s,2);
+
+/*JEU*/
     while(!jouer(s,2,p)){
         affiche(p,s,2);
     }
