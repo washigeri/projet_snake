a99e7d9a86bc5d8ec9f21ebfe73f1f6b11485451
washigeri <malek.elouerghi@gmail.com>
2016-03-07 14:43:20 +0100

    modif affichages

--- src/affichage.h
+++
@@ -1,9 +0,0 @@
-#ifndef AFFICHAGE_H_INCLUDED
-#define AFFICHAGE_H_INCLUDED
-
-bool check(bool* b, int n);
-bool* cherche_snake(int i, int j, snake* s, int n);
-bool* cherche_tete(int i,int j, snake* s, int n);
-void affiche(plateau p, snake* s,int n);
-
-#endif // AFFICHAGE_H_INCLUDED
