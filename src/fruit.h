369da84f79f589644ccfd4fc32e3968b7d1e94f8
marcolinobe <bergdollmarc04@gmail.com>
2016-05-12 10:06:47 +0200

    ajout fruit.h

---
+++ src/fruit.h
@@ -0,0 +1,10 @@
+/** Gestion des Bonus
+ * */
+void ajoutFruit(coord cor,plateau plat);
+
+void retirerFruit(coord cor,plateau plat);
+
+void placerFruit(plateau plat);
+
+int detectionFruit(coord c,plateau plat);
+
